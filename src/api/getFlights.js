7db7f74bb6ef28ab75470feb4e1697ea32a804d0
Yusuf <79724222+altundalyusuf@users.noreply.github.com>
2024-01-21 17:11:00 +0300

    api updated

--- src/api/getFlights.js
+++ src/api/getFlights.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 async function getFlights() {
     try {
         // Fetch flight information from the server
-        const response = await axios.get('http://localhost:3000/flights');
+        const response = await axios.get('https://altundalyusuf.github.io/flight-search-api/flights.json');
         return response.data;
     } catch (error) {
         console.error('Sunucu tarafında hata! Uçuşlar listelenemedi: ', error);
